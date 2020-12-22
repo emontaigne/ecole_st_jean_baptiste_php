3a9f65017de1684f69332b0f2c23e1fb99470bc3
mangbaujessica <mangbaujessica@hotmail.com>
2020-12-23 00:21:00 +0100

    page gallery

--- view/html/footer.php
+++ view/html/footer.php
@@ -7,6 +7,7 @@
 </div></footer>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src = "public/js/accueil.js"></script>
+<script src = "public/js/gallery-lightbox.js"></script>
 <!-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> -->
 
 
---
+++ view/page/galerie.php
@@ -0,0 +1,264 @@
+<div class="mainTotalGallery">
+    <div class="mainGallery">
+    <div class="titlePageGallery">
+    <h2>Galerie photo</h2>
+    <p>Voici la galerie photo</p>
+    </div>
+      <!-- Première gallery photo-->
+      
+      <div class="containerGallery">
+        <div id="activite1Gallery">
+          <a href="#"
+            ><h3 class="titreGallery">
+              Promenade dans les bois <i id="fleche" class="fas fa-sort-down"></i
+              ></i></h3
+          ></a>
+        </div>
+        <div class="galleryImg d-flex" id="gallery1">
+    
+          <a
+            class="example-image-link"
+            href="public/images/photo7.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo7.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo8.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo8.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo9.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo9.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo10.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo10.jpg" alt=""
+          /></a>
+        </div>
+      </div>
+
+      <!-- Deuxième gallery photo-->
+
+      <div class="containerGallery">
+        <div id="activite2Gallery">
+          <a href="#"
+            ><h3 class="titreGallery">
+              Classes vertes <i class="fas fa-sort-down"></i></h3
+          ></a>
+        </div>
+        <div class="galleryImg" id="gallery2">
+          <a
+            class="example-image-link"
+            href="public/images/photo1.jpg"
+            data-lightbox="example-set"
+            data-title="Click the right half of the image to move forward."
+            ><img class="example-image" src="public/images/photo1.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo2.jpg"
+            data-lightbox="example-set"
+            data-title="Or press the right arrow on your keyboard."
+            ><img class="example-image" src="public/images/photo2.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo3.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+            ><img class="example-image" src="../dist/images/photo3.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo4.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo4.jpg" alt=""
+          /></a>
+
+          <a
+            class="example-image-link"
+            href="public/images/photo5.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo5.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo6.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="../dist/images/photo6.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo7.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo7.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo8.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo8.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo9.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo9.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo10.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo10.jpg" alt=""
+          /></a>
+        </div>
+      </div>
+
+      <!-- Troisième gallery photo-->
+
+      <div class="containerGallery">
+        <div id="activite3Gallery">
+          <a href="#"
+            ><h3 class="titreGallery">
+              Activités en classes
+              <i id="sort-down" class="fas fa-sort-down"></i
+              ></h3
+          ></a>
+        </div>
+        <div class="galleryImg" id="gallery3">
+          <a
+            class="example-image-link"
+            href="public/images/photo1.jpg"
+            data-lightbox="example-set"
+            data-title="Click the right half of the image to move forward."
+            ><img class="example-image" src="public/images/photo1.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo2.jpg"
+            data-lightbox="example-set"
+            data-title="Or press the right arrow on your keyboard."
+            ><img class="example-image" src="public/images/photo2.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo3.jpg"
+            data-lightbox="example-set"
+            data-title="The next image in the set is preloaded as you're viewing."
+            ><img class="example-image" src="public/images/photo3.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo4.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo4.jpg" alt=""
+          /></a>
+
+          <a
+            class="example-image-link"
+            href="public/images/photo5.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo5.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo6.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo6.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="ublic/images/photo7.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo7.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo8.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo8.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo9.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo9.jpg" alt=""
+          /></a>
+          <a
+            class="example-image-link"
+            href="public/images/photo10.jpg"
+            data-lightbox="example-set"
+            data-title="Click anywhere outside the image or the X to the right to close."
+            ><img class="example-image" src="public/images/photo10.jpg" alt=""
+          /></a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+  
+
+var btnGallery1 = document.getElementById('activite1Gallery');
+var gallery1 = document.getElementById('gallery1');
+
+btnGallery1.addEventListener('click', (e) => {
+    console.log('toto');
+    e.preventDefault();
+    if (gallery1.style.display !== 'flex') {
+      gallery1.style.display = 'flex';
+    } else {
+      gallery1.style.display = 'none';
+    }
+  });
+  
+  
+var btnGallery2 = document.getElementById('activite2Gallery');
+var gallery2 = document.getElementById('gallery2');
+
+btnGallery2.addEventListener('click', (e) => {
+    console.log('toto');
+    e.preventDefault();
+    if (gallery2.style.display !== 'block') {
+      gallery2.style.display = 'block';
+    } else {
+      gallery2.style.display = 'none';
+    }
+  });
+
+  var btnGallery3 = document.getElementById('activite3Gallery');
+var gallery3 = document.getElementById('gallery3');
+
+btnGallery3.addEventListener('click', (e) => {
+    console.log('toto');
+    e.preventDefault();
+    if (gallery3.style.display !== 'block') {
+      gallery3.style.display = 'block';
+    } else {
+      gallery3.style.display = 'none';
+    }
+  });
+  </script>
