cd724a2946b7b105c553aa5d5752144c3fe77ff4
aurelie-castro <acdemarly@gmail.com>
2020-12-22 22:20:14 +0100

    applied layout to apropos page

--- view/page/apropos.php
+++ view/page/apropos.php
@@ -1,1 +1,41 @@
-<p class="apropos-test">Hello world!</p>
+<div class="header-img" id="header-img"></div>
+<div class="div-rose">
+    <a href=""><h1>Apropos</h1></a>
+</div>
+
+<!-- <p class="apropos-test">Hello world!</p> -->
+<div class="texte-info">
+<div class="info-onglet">
+    <h3><a name="">Apropos</a></h3>
+    <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
+        quibusdam perferendis, obcaecati aperiam, earum voluptates aliquid
+        praesentium eum pariatur eligendi, rem officia porro vero a sapiente
+        error iusto neque odit.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. In
+        temporibus consectetur dicta aliquid, iusto laborum rem praesentium
+        fugiat nam aut exercitationem, saepe corrupti, mollitia dignissimos
+        libero fugit! Ex, culpa consectetur?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+        doloribus dolorum eius a, quisquam recusandae distinctio amet
+        aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
+        libero tempore assumenda!
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+        doloribus dolorum eius a, quisquam recusandae distinctio amet
+        aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
+        libero tempore assumenda!
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+        doloribus dolorum eius a, quisquam recusandae distinctio amet
+        aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
+        libero tempore assumenda!
+    </p>
+</div>
+</div>
