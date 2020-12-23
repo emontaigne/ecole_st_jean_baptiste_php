2b31720fe1aee58cfffc64060ba0eb68d6cb9d6f
aurelie-castro <acdemarly@gmail.com>
2020-12-23 17:26:38 +0100

    implemented new improved buttons to page apropos

--- view/page/apropos.php
+++ view/page/apropos.php
@@ -1,57 +1,65 @@
 <div class="header-img-apropos" id="header-img"></div>
 <div class="div-rose">
-    <a href=""><h1>Apropos</h1></a>
+    <a href="">
+        <h1>Apropos</h1>
+    </a>
 </div>
 
-<div class="cards-info">
-  <a href="?section=histoire" style="text-decoration: none">
-    <div class="card-info activites" id="activites-button" style="border:10px solid 01969b;">
-      <img src="public/images/school-sm-crop.jpg" alt="" />
-      <h3>Histoire de l'école</h3>
-      <p>lorem ipsum</p>
-    </div>
-  </a>
-  <a href="?section=motdirection" style="text-decoration: none">
-    <div class="card-info roi" id="roi-button">
-      <img src="public/images/info3.png" alt="" />
-      <h3>Mot de la direction</h3>
-      <p>lorem ipsum</p>
-    </div>
-  </a>
+
+<div class="buttons-testing-1">
+    <a href="?section=histoire">
+        <div class="button-testing-1 button-histoire">
+            <div class="button-testing-image-histoire"></div>
+            <div class="button-testing-text">
+                <h3>Histoire de l'école</h3>
+                <p>lorem ipsum</p>
+            </div>
+        </div>
+    </a>
+
+    <a href="?section=motdirection">
+        <div class="button-testing-1 button-mot">
+            <div class="button-testing-image-mot"></div>
+            <div class="button-testing-text">
+                <h3>Mot de la direction</h3>
+                <p>lorem ipsum</p>
+            </div>
+        </div>
+    </a>
 </div>
 
 <!-- <p class="apropos-test">Hello world!</p> -->
 <div class="texte-info">
-<div class="info-onglet">
-    <h3><a name="">Apropos</a></h3>
-    <p>Notre projet d’établissement définit l’ensemble
-de nos choix pédagogiques et de nos actions concrètes particulières
-que notre équipe éducative entend mettre en œuvre en collaboration
-avec l’ensemble des acteurs et partenaires pour réaliser le projet éducatif et pédagogique de l’école.
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. In
-        temporibus consectetur dicta aliquid, iusto laborum rem praesentium
-        fugiat nam aut exercitationem, saepe corrupti, mollitia dignissimos
-        libero fugit! Ex, culpa consectetur?
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-        doloribus dolorum eius a, quisquam recusandae distinctio amet
-        aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
-        libero tempore assumenda!
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-        doloribus dolorum eius a, quisquam recusandae distinctio amet
-        aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
-        libero tempore assumenda!
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-        doloribus dolorum eius a, quisquam recusandae distinctio amet
-        aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
-        libero tempore assumenda!
-    </p>
-</div>
+    <div class="info-onglet">
+        <h3><a name="">Apropos</a></h3>
+        <p>Notre projet d’établissement définit l’ensemble
+            de nos choix pédagogiques et de nos actions concrètes particulières
+            que notre équipe éducative entend mettre en œuvre en collaboration
+            avec l’ensemble des acteurs et partenaires pour réaliser le projet éducatif et pédagogique de l’école.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. In
+            temporibus consectetur dicta aliquid, iusto laborum rem praesentium
+            fugiat nam aut exercitationem, saepe corrupti, mollitia dignissimos
+            libero fugit! Ex, culpa consectetur?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+            doloribus dolorum eius a, quisquam recusandae distinctio amet
+            aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
+            libero tempore assumenda!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+            doloribus dolorum eius a, quisquam recusandae distinctio amet
+            aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
+            libero tempore assumenda!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+            doloribus dolorum eius a, quisquam recusandae distinctio amet
+            aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
+            libero tempore assumenda!
+        </p>
+    </div>
 </div>
