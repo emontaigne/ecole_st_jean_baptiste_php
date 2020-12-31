957782c6602089c741f9564fe3f307905d681a30
mangbaujessica <mangbaujessica@hotmail.com>
2020-12-31 03:03:10 +0100

    responsive page covid

--- view/page/covid.php
+++ view/page/covid.php
@@ -2,10 +2,10 @@
     <div class="div-rose">
       <h1>Mesures sanitaires liées à la pandemie Covid-19</h1>
     </div>
-<div class="texte-info">
+<div class="texte-info-covid">
       <div class="code-covid">
-        <h2 class="page-covid">Code actuel: Orange</h2>
-        <p class="page-covid">Dernière mise à jour : le 26 Octobre 2020</p>
+        <h2 class="page-covid-title codeActuel">Code actuel: Orange</h2>
+        <p class="page-covid-title">Dernière mise à jour : le 26 Octobre 2020</p>
       </div>
       <div class="covid-cards">
         <div class="card-covid one">
@@ -15,8 +15,8 @@
           <div class="card-covid texte" style="color:black;">
             <p class="page-covid">
               Le port du masque est obligatoire pour tous les adultes et
-              enseignants se trouveant dans l'établissement. Les élèves ne
-              portent pas le masque.
+              enseignants se trouveant dans l'établissement. 
+              <br>Les élèves ne portent pas le masque.
             </p>
           </div>
         </div>
@@ -27,8 +27,8 @@
           </div>
           <div class="card-covid texte">
             <p class="page-covid">
-              Du gel désinfectant est mis à disposition du personnel. Les élèves
-              se nettoient également regulièrement les mains.
+              Du gel désinfectant est mis à disposition du personnel. 
+              <br>Les élèves se nettoient également regulièrement les mains.
             </p>
           </div>
         </div>
@@ -51,19 +51,20 @@
           </div>
           <div class="card-covid texte">
             <p class="page-covid">
-              En cas de symptômes, il est recommandé de rester à la maison. Une
-              période de quarantaine de 10 jours est de rigueur.
+              En cas de symptômes, il est recommandé de rester à la maison. 
+              <br>Une période de quarantaine de 10 jours est de rigueur.
             </p>
           </div>
         </div>
 
         <div class="card-covid cinq">
           <div>
-            <img src="public/images/playing.png" alt="" />
+            <img src="public/images/mask-01.png"  class="icon-covid" alt="" />
           </div>
-          <div class="card-texte">
+          <div class="card-covid -texte">
             <p class="page-covid">
-              Les activités extra-scolaires sont annulées. Cependant, les
+              Les activités extra-scolaires sont annulées. 
+              <br>Cependant, les
               enfants sont autorisés à jouer dans la cour de récréation.
             </p>
           </div>
