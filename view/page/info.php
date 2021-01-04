2cf950bb439fea99b974d384f5e8e1e855526aa4
aurelie-castro <acdemarly@gmail.com>
2021-01-04 22:34:48 +0100

    fixed dates in info generales and changed font globally

--- view/page/info.php
+++ view/page/info.php
@@ -36,25 +36,44 @@
         <ul>
           <li>Fête de la Communauté Française</li>
           <li>Vacances de Toussaint</li>
-          <li>Communication du 11 novembre</li>
+          <li>Commémoration du 11 novembre</li>
           <li>Veillée de Noël</li>
           <li>Vacances de Noël</li>
           <li>Souper</li>
           <li>Vacances de Carnaval</li>
           <li>Vacances de Pâques</li>
           <li>Congé</li>
+          <li>Fête du travail</li>
+          <li>Fancy-Fair</li>
+          <li>Ascension</li>
+          <li>Congé</li>
+          <li>Pentecôte</li>
+          <li>Vacances d'Eté</li>
+          <li>Journées pédagogiques</li>
+          <li>Réunions des parents</li>
         </ul>
       </div>
       <div class="dates-2020" id="dates-2020">
         <ul>
-          <li>Dimanche 27 Septembre 2020</li>
-          <li>Lundi 2 au vendredi 6 novembre</li>
-          <li>Mercredi 11 novembre 2020</li>
-          <li>Jeudi 17 décembre</li>
-          <li>Lundi 21 décembre au vendredi 1er janvier</li>
-          <li>Samedi 27 février 2021</li>
-          <li>Lundi 1er avril au vendredi 7 avril 2021</li>
-          <li>Vendredi 10 avril 2021</li>
+          <li>27 Septembre</li>
+          <li>2 au 6 novembre</li>
+          <li>11 novembre</li>
+          <li>17 décembre</li>
+          <li>21 décembre au 1er janvier</li>
+          <li>27 février 2021</li>
+          <li>15 février au 19 février 2021</li>
+          <li>5 avril au 16 avril 2021.</li>
+          <li>30 avril 2021</li>
+          <li>1er mai 2021</li>
+          <li>8 mai 2021</li>
+          <li>13 mai 2021</li>
+          <li>14 mai 2021</li>
+          <li>24 mai 2021</li>
+          <li>1er juillet au lundi 31 août 2021</li>
+          <li>19/11, 01/02, 08/03, 31/05</li>
+          <li> 15/09 à 19h (maternelle), 22/09 à 19h (primaire)</li>
+
+
         </ul>
       </div>
     </div>
