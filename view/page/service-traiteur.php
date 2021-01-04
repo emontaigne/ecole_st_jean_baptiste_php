ae80b14b3cb68f9d8e1ec3501c3b5a55a05073d7
mangbaujessica <mangbaujessica@hotmail.com>
2021-01-04 17:38:04 +0100

    update page repas

--- view/page/service-traiteur.php
+++ view/page/service-traiteur.php
@@ -1,30 +1,25 @@
-<div class="header-img-service-traiteur" id="header-img"></div>
-<div class="div-rose service-traiteur-rose">
-    <a href="">
-        <h1 class="traiteur-titre">Service traiteur</h1>
-    </a>
-
-    <div class="fleche-retour-traiteur"><a href="?section=accueil">Home</a>><a href="?section=info"> Informations générales</a>><a href="?section=service-traiteur">Service traiteur</a></div>
+<div class="containerTraiteur">
+<div class="boxTraiteur">
+    <div class="div-avec-titre">
+<h1 class="service-Traiteur-Titre">Service traiteur</h1>
 </div>
+    <div class="traiteur-texte">
 
-<div class="texte-info-traiteur">
-<div class="info-onglet" id="activites">
-    <h3><a name="acti">Le service traiteur</a></h3>
-        <p class="para-service-traiteur">
-        Depuis plusieurs années, un service traiteur offre le service de repas chauds aux élèves et au personnel de l'école.Vous avez la possibilité de visualier en ligne le menu des repas pour votre enfant dans la section privée.
+        
+   
+    <p >
+        Depuis plusieurs années, un service traiteur offre le service de repas chauds aux élèves et au personnel de l'école.
            <br>
-           Les repas sont élaborés par des techniciennes en diététiques. Ils respectent tous les éléments essentiels recommandé pour un bon développement et une bonne santé et sont approuvées par des nutritionnistes chevronnées.</p>
-           <p class="para-service-traiteur">
+        
            Chaque jour de classe, une dizaine d’employés se déploient dans les cuisines, les salles de repas ainsi que sur la route afin que plus jamais, étudier le ventre vide ne soit une option.
             </p>
 
-            <p class="para-service-traiteur">
+            <p >
             Chaque élève reçoit tous les éléments essentiels pour un bon développement physique et intellectuel. Nous prenons une attention particulière afin d’inclure de nouveaux aliments sains et riches en nutriments. Tout est pensé de façon à faire prendre conscience aux élèves l’importance de prendre de bonnes habitudes alimentaires.
             </p>
-            <div class="service-traiteur-image">
-                <img class="img-service-traiteur"src="public/images/reservation-repas-vert.jpg" alt="">
-
             </div>
-   
+     
+            
 </div>
 </div>
+
