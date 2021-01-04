30a2aaa9aa17f337f93db14d502803fbd6b2dcdb
aurelie-castro <acdemarly@gmail.com>
2021-01-04 17:35:31 +0100

    added text content and style to apropos, roi and histoire pages

--- view/page/histoire.php
+++ view/page/histoire.php
@@ -8,25 +8,24 @@
 </div>
 
 <div class="texte-info histoire">
-    <div class="info-onglet" id="ephemerides">
+    <div class="info-onglet onglet-histoire" id="ephemerides">
         <h3><a name="ephe">Notre Philosophie</a></h3>
-        <p>Un milieu riche en découvertes où chacun profite d’un cadre rassurant, épanouissant et bienveillant.</p>
+        <p>L'école Saint Jean-Baptiste aspire à être un milieu riche en découvertes où chacun profite d’un cadre rassurant, épanouissant et bienveillant.</p>
         <p>L’enfant-acteur peut :</p>
         <ul>
             <li>Vivre</li>
             <li>Exploiter sa curiosité naturelle</li>
             <li>S’intéresser aux manifestations de la vie.</li>
-        </ul>
+
 
         <p>Un milieu qui :</p>
-        <ul>
+
             <li>Suscite et développe chez l’enfant le goût, la joie et le désir d’apprendre.</li>
             <li>Développe la tolérance et l’adaptation.</li>
             <li>Sensibilise les enfants à la vie qui les entoure et aux responsabilités qu’ils ont par rapport à celle-ci.</li>
-        </ul>
 
-        <p>A l’école Anne Marie, on entre dans :</p>
-        <ul>
+
+        <p>A l’école Saint Jean-Baptiste, on entre dans :</p>
             <li>Une école familiale, accueillante et chaleureuse où l’on cultive l’art de vivre ensemble dans le respect des différences.</li>
             <li>Une école de la réussite, ou les enseignantes se remettent sans cesse en recherche pédagogique (formations, différenciation, continuité dans les apprentissages)</li>
             <li>La pratique de pédagogies alternatives. L’enfant y est acteur de ses apprentissages.</li>
@@ -38,30 +37,6 @@
 
         <p>L’éveil de l’enfant étant parmi nos priorités, nous offrons un accueil extrascolaire de qualité et varié.</p>
         <p>Cette année nous avons proposé des ateliers musicaux, des activités de massages, de jardinage et d’anglais.
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. In
-            temporibus consectetur dicta aliquid, iusto laborum rem praesentium
-            fugiat nam aut exercitationem, saepe corrupti, mollitia dignissimos
-            libero fugit! Ex, culpa consectetur?
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-            doloribus dolorum eius a, quisquam recusandae distinctio amet
-            aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
-            libero tempore assumenda!
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-            doloribus dolorum eius a, quisquam recusandae distinctio amet
-            aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
-            libero tempore assumenda!
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-            doloribus dolorum eius a, quisquam recusandae distinctio amet
-            aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
-            libero tempore assumenda!
         </p>
     </div>
 </div>
