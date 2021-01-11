960671a379bf7fd413f40c34ac8a8fcde250ce3c
aurelie-castro <acdemarly@gmail.com>
2021-01-11 13:28:03 +0100

    implemented activity cards version 1

--- view/page/activites.php
+++ view/page/activites.php
@@ -1,45 +1,120 @@
-<div class = "container2">
-<div class="header-img-acti" id="header-img"></div>
-<div class="div-rose activites-rose">
-    <a href="">
-        <h1 class="activ-titre">Activités</h1>
-    </a>
+<div class="container2">
+    <div class="header-img-acti" id="header-img"></div>
+    <div class="div-rose activites-rose">
+        <a href="">
+            <h1 class="activ-titre">Activités</h1>
+        </a>
 
-    <div class="fleche-retour-info"><a href="?section=accueil">Accueil</a>><a href="?section=info"> Informations générales</a>><a href="?section=activites">Activités</a></div>
-</div>
+        <div class="fleche-retour-info"><a href="?section=accueil">Accueil</a>><a href="?section=info"> Informations générales</a>><a href="?section=activites">Activités</a></div>
+    </div>
 
-<div class="texte-info-activites">
-<div class="info-onglet" id="activites">
-    <h3><a name="acti">Les activités</a></h3>
-    <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
-        quibusdam perferendis, obcaecati aperiam, earum voluptates aliquid
-        praesentium eum pariatur eligendi, rem officia porro vero a sapiente
-        error iusto neque odit.
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. In
-        temporibus consectetur dicta aliquid, iusto laborum rem praesentium
-        fugiat nam aut exercitationem, saepe corrupti, mollitia dignissimos
-        libero fugit! Ex, culpa consectetur?
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-        doloribus dolorum eius a, quisquam recusandae distinctio amet
-        aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
-        libero tempore assumenda!
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-        doloribus dolorum eius a, quisquam recusandae distinctio amet
-        aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
-        libero tempore assumenda!
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-        doloribus dolorum eius a, quisquam recusandae distinctio amet
-        aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
-        libero tempore assumenda!
-    </p>
-</div>
-</div>
+    <div class="texte-info-activites">
+
+        <div class="activity-cards-info">
+            <a>
+                <div class="activity-card-info acti-ponctuelle">
+                    <div class="activity-card-info-img"></div>
+                    <div class="activity-card-info-text">
+                        <h3>Activité</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat veritatis accusantium id fugit doloremque, ea maiores! Fugiat, molestiae repellendus! Maiores.</p>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+
+        <div class="activity-cards-info">
+            <a>
+                <div class="activity-card-info acti-ponctuelle">
+                    <div class="activity-card-info-img"></div>
+                    <div class="activity-card-info-text">
+                        <h3>Activité</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat veritatis accusantium id fugit doloremque, ea maiores! Fugiat, molestiae repellendus! Maiores.</p>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+
+        <div class="activity-cards-info">
+            <a>
+                <div class="activity-card-info acti-ponctuelle">
+                    <div class="activity-card-info-img"></div>
+                    <div class="activity-card-info-text">
+                        <h3>Activité</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat veritatis accusantium id fugit doloremque, ea maiores! Fugiat, molestiae repellendus! Maiores.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="activity-cards-info">
+            <a>
+                <div class="activity-card-info acti-permanente">
+                    <div class="activity-card-info-img"></div>
+                    <div class="activity-card-info-text">
+                        <h3>Activité</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat veritatis accusantium id fugit doloremque, ea maiores! Fugiat, molestiae repellendus! Maiores.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="activity-cards-info">
+            <a>
+                <div class="activity-card-info acti-verte">
+                    <div class="activity-card-info-img"></div>
+                    <div class="activity-card-info-text">
+                        <h3>Activité</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat veritatis accusantium id fugit doloremque, ea maiores! Fugiat, molestiae repellendus! Maiores.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="activity-cards-info">
+            <a>
+                <div class="activity-card-info acti-neige">
+                    <div class="activity-card-info-img"></div>
+                    <div class="activity-card-info-text">
+                        <h3>Activité</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat veritatis accusantium id fugit doloremque, ea maiores! Fugiat, molestiae repellendus! Maiores.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="info-onglet" id="activites">
+            <h3><a name="acti">Les activités</a></h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
+                quibusdam perferendis, obcaecati aperiam, earum voluptates aliquid
+                praesentium eum pariatur eligendi, rem officia porro vero a sapiente
+                error iusto neque odit.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. In
+                temporibus consectetur dicta aliquid, iusto laborum rem praesentium
+                fugiat nam aut exercitationem, saepe corrupti, mollitia dignissimos
+                libero fugit! Ex, culpa consectetur?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                doloribus dolorum eius a, quisquam recusandae distinctio amet
+                aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
+                libero tempore assumenda!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                doloribus dolorum eius a, quisquam recusandae distinctio amet
+                aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
+                libero tempore assumenda!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                doloribus dolorum eius a, quisquam recusandae distinctio amet
+                aspernatur ut ullam corporis ea ipsum nesciunt, quidem ipsa facere
+                libero tempore assumenda!
+            </p>
+        </div>
+    </div>
